aca3492f5d1bc94e8c89e340e51d32d84fa272c6
pkluiber <pkluiber@gmail.com>
2021-09-27 15:53:05 +0200

    kluiber

---
+++ EX2/sort.js
@@ -0,0 +1,6 @@
+const array = [1, 7, -3, 9];
+array.sort(function(a, b) { return a - b });
+
+document.write(array)
+
+console.log(Math.max(1, 7, -3, 9))
---
+++ Temepratur EX1/tempvar.js
@@ -0,0 +1,16 @@
+var temperatur = Math.floor(Math.random() * 50 - 5);
+
+
+
+var temp = temperatur
+
+if (temp > 31) {
+    console.log("The weather is hot");
+} else
+if (temp < 11) {
+    console.log("The weather is cold");
+} else {
+    console.log("The weather is moderate");
+}
+
+document.write(temperatur);
